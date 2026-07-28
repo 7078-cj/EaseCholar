ae38b9735499fbdd7688aac23673b93d2274cc4b
7078-cj <7078ceejay@gmail.com>
2026-07-29 03:20:13 +0800

    feat: refactor authentication and routing structure; remove unused components

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,4 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 from .Views.user_views import extract_and_match,test
 
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Login from './Pages/Login'
-import PrivateRoutes from './Contexts/PrivateRoutes'
-import Register from './Pages/Register'
-import { AuthProvider } from './Contexts/AuthContext'
-import DashBoard from './Pages/DashBoard'
 
 
 
@@ -15,17 +10,11 @@ function App() {
     <>
       
       <Router>
-        <AuthProvider>
+        
           <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-              
-            <Route element={<PrivateRoutes />} >
-              <Route path="/" element={<DashBoard />} />
-            </Route>
+            <Route path="/" element={<Home />} />
               
           </Routes>
-        </AuthProvider>
       </Router>
       
       
